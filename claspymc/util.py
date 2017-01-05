24575e5b8573c8d1939f4b02c1cd29fa2a513a34
Thomas Bell <tom.aus@outlook.com>
2017-01-05 15:56:25 +0800

    Hexdumps are now more fancy, incremented version
    * Offset issue fixed
    * If three or more consecutive lines are the same, all but one are omitted

--- claspymc/util.py
+++ claspymc/util.py
@@ -20,11 +20,39 @@ def data_filename(server, filename):
     return os.path.join(directory, filename)
 
 
-def print_hex_dump(contents):
+def print_hex_dump(contents,
+                   print_all=False,
+                   format_line=None,
+                   omit_line=None):
+
+    def _format_line(offset, data):
+        return "| {:02x} {:04x} | {: <47} | {: <16} |".format(
+            (offset >> 16) & 0xFF,
+            offset & 0xFFFF,
+            ' '.join('{:02x}'.format(x) for x in data),
+            ''.join((chr(x) if (0x20 <= x < 0x7f) else '.') for x in data))
+
+    if format_line is None:
+        format_line = _format_line
+
+    if omit_line is None:
+        omit_line = "|         |                                                 |                  |"
+
+    previous_row = None
+    omitted = 0
     for pos in range(0x0000, len(contents), 16):
         row = contents[pos:pos+16]
-        print("{:04x} {:04x}: {: <47} | {: <16} |".format(
-            (pos << 16) & 0xFFFF,
-            pos & 0xFFFF,
-            ' '.join('{:02x}'.format(x) for x in row),
-            ''.join((chr(x) if (0x20 <= x < 0x7f) else '.') for x in row)))
+        if row != previous_row or print_all:
+            if omitted != 0:
+                if omitted > 1:
+                    print(omit_line)
+                else:
+                    print(format_line(pos-16, previous_row))
+
+            print(format_line(pos, row))
+
+            omitted = 0
+        else:
+            omitted += 1
+
+        previous_row = row
